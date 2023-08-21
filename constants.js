16f224041b215aee78a11410a7efac2fea769510
Tranquocbao <quocbao.tran@manabie.com>
2023-08-21 10:44:24 +0700

    update padding mail template

--- constants.js
+++ constants.js
@@ -19,51 +19,58 @@ https://w.ladicdn.com/s250x250/60a651b7c4ec0f001367452b/header-nepcon-2023081407
           data-bit="iit"
         />
       </p>
-      <h1 style="margin-left: 40px">
-        <span style="font-size: 26px"
-          >Dear {{contact_title}} {{contact_first_name}}</span
-        >
-      </h1>
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >Your Registration is now completed!</span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px">Đăng ký của Quý Vị đã hoàn tất!</span>
-      </p>
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >Welcome you to visit <strong>NEPCON Vietnam 2023</strong>. Please
-          find it here in attached is your QR code for your onsite badge print
-          and check in.</span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          >Chào mừng Quý Khách đến tham quan Triển Lãm
-          <strong>NEPCON Việt Nam 2023</strong>. Đính kèm trong email mà mã QR
-          của Quý Khách, vui lòng sử dụng mã này để in thẻ định danh và check
-          in tại sự kiện.</span
-        >
-      </p>
-      <table cellpadding="1" cellspacing="1" style="width: 600px">
+      <div style="padding: 0px 40px">
+        <h1>
+          <span style="font-size: 26px"
+            >Dear {{contact_title}} {{contact_first_name}}</span
+          >
+        </h1>
+        <p>
+          <span style="font-size: 16px"
+            >Your Registration is now completed!</span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px">Đăng ký của Quý Vị đã hoàn tất!</span>
+        </p>
+        <p>
+          <span style="font-size: 16px"
+            >Welcome you to visit <strong>NEPCON Vietnam 2023</strong>. Please
+            find it here in attached is your QR code for your onsite badge
+            print and check in.</span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            >Chào mừng Quý Khách đến tham quan Triển Lãm
+            <strong>NEPCON Việt Nam 2023</strong>. Đính kèm trong email mà mã
+            QR của Quý Khách, vui lòng sử dụng mã này để in thẻ định danh và
+            check in tại sự kiện.</span
+          >
+        </p>
+      </div>
+
+      <table
+        cellpadding="1"
+        cellspacing="1"
+        style="width: 600px; padding: 0px 40px"
+      >
         <tbody>
           <tr>
             <td>
-              <p style="margin-left: 40px">
+              <p>
                 <span style="font-size: 16px"
                   >[Full name | Họ & Tên]:
                   <strong>{{contact_full_name}}</strong></span
                 >
               </p>
-              <p style="margin-left: 40px">
+              <p>
                 <span style="font-size: 16px"
                   >[Company name | Tên công ty]:
                   <strong>{{company_name}}</strong></span
                 >
               </p>
-              <p style="margin-left: 40px">
+              <p>
                 <span style="font-size: 16px"
                   >[Email]:
                   <strong
@@ -91,47 +98,49 @@ https://w.ladicdn.com/s250x250/60a651b7c4ec0f001367452b/header-nepcon-2023081407
           </tr>
         </tbody>
       </table>
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >For any questions regarding your registration, please email us at
-          <em
-            ><a
-              href="mailto:
+      <div style="padding: 0px 40px">
+        <p>
+          <span style="font-size: 16px"
+            >For any questions regarding your registration, please email us at
+            <em
+              ><a
+                href="mailto:
 contactcenter@rxtradex.com"
-              target="_blank"
-              >contactcenter@rxtradex.com</a
-            ></em
-          ></span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          >Mọi yêu cầu hoặc thắc mắc cần giải đáp, vui lòng liên hệ với bộ
-          phận CSKH của chúng tôi qua email
-          <em
-            ><a
-              href="mailto:
+                target="_blank"
+                >contactcenter@rxtradex.com</a
+              ></em
+            ></span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            >Mọi yêu cầu hoặc thắc mắc cần giải đáp, vui lòng liên hệ với bộ
+            phận CSKH của chúng tôi qua email
+            <em
+              ><a
+                href="mailto:
 contactcenter@rxtradex.com"
-              target="_blank"
-              >contactcenter@rxtradex.com</a
-            ></em
-          ></span
-        >
-      </p>
-      <hr />
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"><em>Sincerely,</em></span>
-      </p>
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          ><strong>Organization Committee</strong></span
-        >
-      </p>
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          ><strong>RX Tradex Vietnam</strong></span
-        >
-      </p>
+                target="_blank"
+                >contactcenter@rxtradex.com</a
+              ></em
+            ></span
+          >
+        </p>
+        <hr />
+        <p>
+          <span style="font-size: 16px"><em>Sincerely,</em></span>
+        </p>
+        <p>
+          <span style="font-size: 16px"
+            ><strong>Organization Committee</strong></span
+          >
+        </p>
+        <p>
+          <span style="font-size: 16px"
+            ><strong>RX Tradex Vietnam</strong></span
+          >
+        </p>
+      </div>
       <p style="text-align: center">
         <img
           alt=""
@@ -147,7 +156,8 @@ https://w.ladicdn.com/s250x250/60a651b7c4ec0f001367452b/footer-nepcon-2023081407
     </td>
   </tr>
 </tbody>
-</table>`;
+</table>
+`;
 
 export const HTMLTemplatev2 = `<table
 align="center"
@@ -170,70 +180,76 @@ style="width: 700px"
         />
       </p>
 
-      <h1 style="margin-left: 40px">
-        <span style="font-size: 26px"
-          >Dear {{contact_title}} {{contact_first_name}}</span
-        >
-      </h1>
+      <div style="padding: 0px 40px">
+        <h1>
+          <span style="font-size: 26px"
+            >Dear {{contact_title}} {{contact_first_name}}</span
+          >
+        </h1>
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >Your <strong>Group Registration</strong> is now completed!</span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          ><strong>Đăng ký Nhóm</strong> của Quý Vị đã hoàn tất!</span
-        >
-      </p>
+        <p>
+          <span style="font-size: 16px"
+            >Your <strong>Group Registration</strong> is now completed!</span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            ><strong>Đăng ký Nhóm</strong> của Quý Vị đã hoàn tất!</span
+          >
+        </p>
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >Welcome you to visit <strong>NEPCON Vietnam 2023</strong>. Please
-          find it here in attached is your QR code for your onsite badge print
-          and check in.</span
-        >
-      </p>
+        <p>
+          <span style="font-size: 16px"
+            >Welcome you to visit <strong>NEPCON Vietnam 2023</strong>. Please
+            find it here in attached is your QR code for your onsite badge
+            print and check in.</span
+          >
+        </p>
 
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          >Chào mừng Quý Khách đến tham quan Triển Lãm
-          <strong>NEPCON Việt Nam 2023</strong>. Đính kèm trong email mà mã QR
-          của Quý Khách, vui lòng sử dụng mã này để in thẻ định danh và check
-          in cho Nhóm tại quầy đăng ký dành cho doanh nghiệp.</span
-        >
-      </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            >Chào mừng Quý Khách đến tham quan Triển Lãm
+            <strong>NEPCON Việt Nam 2023</strong>. Đính kèm trong email là mã
+            QR Nhóm của Quý Khách, vui lòng sử dụng mã này để in thẻ định danh
+            và check in cho Nhóm tại quầy đăng ký dành cho doanh nghiệp.</span
+          >
+        </p>
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >Here is the team leader information:</span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          >Dưới đây là thông tin đăng ký của trưởng đoàn:</span
-        >
-      </p>
+        <p>
+          <span style="font-size: 16px"
+            >Here is the team leader information:</span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            >Dưới đây là thông tin đăng ký của trưởng đoàn:</span
+          >
+        </p>
+      </div>
 
-      <table cellpadding="1" cellspacing="1" style="width: 600px">
+      <table
+        cellpadding="1"
+        cellspacing="1"
+        style="width: 600px; padding: 0px 40px"
+      >
         <tbody>
           <tr>
             <td>
-              <p style="margin-left: 40px">
+              <p>
                 <span style="font-size: 16px"
                   >[Full name | Họ & Tên]:
                   <strong>{{contact_full_name}}</strong></span
                 >
               </p>
 
-              <p style="margin-left: 40px">
+              <p>
                 <span style="font-size: 16px"
                   >[Company name | Tên công ty]:
                   <strong>{{company_name}}</strong></span
                 >
               </p>
 
-              <p style="margin-left: 40px">
+              <p>
                 <span style="font-size: 16px"
                   >[Email]:
                   <strong
@@ -262,59 +278,61 @@ style="width: 700px"
         </tbody>
       </table>
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >If you wish to print identification badges and check-in for
-          individual, please refer to the attached PDF file in this email to
-          obtain the QR code for each person.</span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          >Nếu Quý Khách muốn in thẻ định danh và check in cho từng cá nhân,
-          vui lòng xem file PDF được đính kèm trong email này để lấy mã QR
-          code cho từng người.</span
-        >
-      </p>
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          >For any questions regarding your registration, please email us at
-          <em
-            ><a href="mailto:contactcenter@rxtradex.com" target="_blank"
-              >contactcenter@rxtradex.com</a
-            ></em
-          ></span
-        >
-      </p>
-      <p style="margin-left: 40px; font-style: italic">
-        <span style="font-size: 16px"
-          >Mọi yêu cầu hoặc thắc mắc cần giải đáp, vui lòng liên hệ với bộ
-          phận CSKH của chúng tôi qua email
-          <em
-            ><a href="mailto:contactcenter@rxtradex.com" target="_blank"
-              >contactcenter@rxtradex.com</a
-            ></em
-          ></span
-        >
-      </p>
+      <div style="padding: 0px 40px">
+        <p>
+          <span style="font-size: 16px"
+            >If you wish to print identification badges and check-in for
+            individual, please refer to the attached PDF file in this email to
+            obtain the QR code for each person.</span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            >Nếu Quý Khách muốn in thẻ định danh và check in cho từng cá nhân,
+            vui lòng xem file PDF được đính kèm trong email này để lấy mã QR
+            code cho từng người.</span
+          >
+        </p>
+        <p>
+          <span style="font-size: 16px"
+            >For any questions regarding your registration, please email us at
+            <em
+              ><a href="mailto:contactcenter@rxtradex.com" target="_blank"
+                >contactcenter@rxtradex.com</a
+              ></em
+            ></span
+          >
+        </p>
+        <p style="font-style: italic">
+          <span style="font-size: 16px"
+            >Mọi yêu cầu hoặc thắc mắc cần giải đáp, vui lòng liên hệ với bộ
+            phận CSKH của chúng tôi qua email
+            <em
+              ><a href="mailto:contactcenter@rxtradex.com" target="_blank"
+                >contactcenter@rxtradex.com</a
+              ></em
+            ></span
+          >
+        </p>
 
-      <hr />
+        <hr />
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"><em>Sincerely,</em></span>
-      </p>
+        <p>
+          <span style="font-size: 16px"><em>Sincerely,</em></span>
+        </p>
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          ><strong>Organization Committee</strong></span
-        >
-      </p>
+        <p>
+          <span style="font-size: 16px"
+            ><strong>Organization Committee</strong></span
+          >
+        </p>
 
-      <p style="margin-left: 40px">
-        <span style="font-size: 16px"
-          ><strong>RX Tradex Vietnam</strong></span
-        >
-      </p>
+        <p>
+          <span style="font-size: 16px"
+            ><strong>RX Tradex Vietnam</strong></span
+          >
+        </p>
+      </div>
 
       <p style="text-align: center">
         <img
