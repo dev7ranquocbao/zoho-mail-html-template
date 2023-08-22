fab5e3dd5c9f376341e00eedde891dc3e39e26ab
Tranquocbao <quocbao.tran@manabie.com>
2023-08-22 19:13:52 +0700

    add emperia encrypt route

--- src/constants/encrypt.ts
+++ src/constants/encrypt.ts
@@ -1,1 +1,1 @@
-export const Secret_EncryptKey = "dev";
+export const Secret_EncryptKey = "aV8qJW9ORV97JSM0aVouZClHYTpLdkI3WA==";
--- src/routes/emperia-encrypt.ts
+++ src/routes/emperia-encrypt.ts
@@ -1,22 +1,54 @@
 import express from "express";
-import AES from "crypto-js/aes";
 import { Secret_EncryptKey } from "../constants/encrypt";
+import CryptoJS from "crypto-js";
 import { logError } from "../utils/logger";
 
 const router = express.Router();
+
+const execute = (
+    type: "encrypt" | "decrypt",
+    message: string,
+    secret: string,
+): string => {
+    switch (type) {
+        case "encrypt": {
+            const passphrase = CryptoJS.enc.Base64.parse(secret).toString(
+                CryptoJS.enc.Utf8,
+            );
+
+            const encrypted = CryptoJS.AES.encrypt(message, passphrase);
+            const cipherText = encrypted.toString();
+
+            return cipherText;
+        }
+
+        case "decrypt": {
+            const passphrase = CryptoJS.enc.Base64.parse(secret).toString(
+                CryptoJS.enc.Utf8,
+            );
+
+            const decrypted = CryptoJS.AES.decrypt(message, passphrase);
+            const plainText = decrypted.toString(CryptoJS.enc.Utf8);
+
+            return plainText;
+        }
+
+        default:
+            return "bad encrypt";
+    }
+};
 
 router.get("", async (req, res) => {
     try {
         const data = req.query.data;
 
         if (typeof data !== "string") {
-            res.status(200).send("");
+            res.status(200).json({ data: "" });
             return;
         }
 
-        const encrypted = AES.encrypt(data, Secret_EncryptKey);
-
-        res.status(200).send(encrypted.toString());
+        const result = execute("encrypt", data, Secret_EncryptKey);
+        res.status(200).json({ data: result });
     } catch (error) {
         logError(error);
         res.status(400).json({ message: error });
