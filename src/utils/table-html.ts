56e378c8e19e1fccba820d6ea31f96695c50ea73
Tranquocbao <quocbao.tran@manabie.com>
2023-10-03 01:11:44 +0700

    fix a exhibitor

--- src/utils/table-html.ts
+++ src/utils/table-html.ts
@@ -1,5 +1,7 @@
 import intersectionBy from "lodash/intersectionBy.js";
 import sampleSize from "lodash/sampleSize.js";
+import shuffle from "lodash/shuffle.js";
+
 import { ExhibitorData } from "../databases/exhibitor-sheet.js";
 import { ExhibitorDataMXV } from "../databases/exhibitor-sheet-mxv.js";
 import { MXVDataStructureRCM } from "../databases/types.js";
@@ -39,6 +41,41 @@ export const makeHTMLTableBodyMXV2023 = (
     showId: MXVDataStructureRCM["ShowId"],
     keywords: string[],
 ) => {
+    const fixedExhibitor: MXVDataStructureRCM = {
+        EventEditionName: "METALEX Vietnam VND",
+        EventEditionID: "eve-49b5aebd-0180-417d-b112-cb412f3e8097",
+        ExhibitorID: "exh-bb04d8fc-298c-475e-ba81-6fd7193c802d",
+        CompanyName: "NEWTON GMBH",
+        DisplayName: "GHM GROUP",
+        ExhibitorStatus: "ACTIVE",
+        ExhibitorType: "EXHIBITOR",
+        PackageName: "Bronze",
+        Stands: "R02",
+        SortOrderAlias: "NEWTON GMBH",
+        ProfileCompleteness: "100",
+        ExhibitorDetailPageURL:
+            "https://www.metalexvietnam.com/en-gb/for-visitor/exhibitor-list/exhibitor-details.org-6000f756-77a8-41f4-881f-a67c0f48e093.html",
+        Description:
+            "The GHM GROUP is a specialist in measurement technology. Our business unit “Industry” offers sensors for flow, level, pressure, temperature, and liquid analysis, the electronics part of the portfolio comprises analytical measuring technology, safety-related assemblies, automation technology, and indicators. Our business unit “Portable” offers a wide range of portable instruments, sensors, and probes. Our business unit “Environmental” focuses on environmental technology, like weather stations, meteo stations, pyranometers, anemomenters, albedimeters, and microclimate data loggers.",
+        WhyVisitOurStand:
+            "We showcase high-quality measurement technology developed and manufactured in Europe. Our product range covers industrial sensors and electronics, portable instruments, and environmental technology.",
+        Brands: "",
+        PPSCompanyActivity: "Manufacturer",
+        PPSIndustryProfile:
+            "Oil & Gas | Chemical | Automotive | Auto Parts | Electric | Electronic | Automation | Machinery",
+        PPSMachineTechnologyProfile: [
+            "Automation",
+            "Tools and Tooling",
+            "Metrology",
+            "Testing device",
+            "Chemical",
+            "Safety Equipment",
+        ],
+        PPSProductsServicesHighlight:
+            "First Time In Vietnam | Industry 4.0 Technology Supported",
+        ShowId: "MXV23",
+    };
+
     const filteredData = ExhibitorDataMXV.filter(exhibitor => {
         return exhibitor.ShowId === showId;
     });
@@ -52,23 +89,25 @@ export const makeHTMLTableBodyMXV2023 = (
         return intersect.length > 0;
     });
 
-    let randomExhibitors = sampleSize(collectData, 10);
+    let randomExhibitors = sampleSize(collectData, 9);
 
     if (randomExhibitors.length < 10) {
-        randomExhibitors = sampleSize(filteredData, 10);
+        randomExhibitors = sampleSize(filteredData, 9);
     }
 
     let trs = "";
 
-    randomExhibitors.forEach((exhibitor, index) => {
-        const { CompanyName, Stands } = exhibitor;
+    shuffle([fixedExhibitor, ...randomExhibitors]).forEach(
+        (exhibitor, index) => {
+            const { CompanyName, Stands } = exhibitor;
 
-        trs += `<tr>
+            trs += `<tr>
             <td>${index + 1}</td>
             <td>${CompanyName}</td>
             <td>${Stands}</td>
         </tr>`;
-    });
+        },
+    );
 
     return `<tbody>${trs}</tbody>`;
 };
